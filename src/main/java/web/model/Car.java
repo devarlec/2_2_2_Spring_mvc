43da2cbb24fdf48654b364aba054b737ef371788
evgen <devarlec@gmail.com>
2025-04-01 04:05:55 +0400

    added model car, service createFiveCar() and controller printCars()

---
+++ src/main/java/web/model/Car.java
@@ -0,0 +1,38 @@
+package web.model;
+
+public class Car {
+
+    private String name;
+    private String model;
+    private String owner;
+
+    public Car(String name, String model, String owner) {
+        this.name = name;
+        this.model = model;
+        this.owner = owner;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+}
